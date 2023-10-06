c17615512f35424fa36424cb0903f0ade9c6084a
ChaoticFS <Magnus.overgaard1711@gmail.com>
2023-10-06 11:24:15 +0200

    Added endpoint for version info

--- BookingHandlerService/Controllers/BookingController.cs
+++ BookingHandlerService/Controllers/BookingController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using BookingHandlerService.Models;
 using System.Text.Json;
 using BookingHandlerService.Messaging;
+using System.Diagnostics;
 
 namespace BookingHandlerService.Controllers;
 
@@ -47,7 +48,31 @@ public class BookingController : ControllerBase
         return Ok(planFileJson);
     }
 
-    private List<PlanDTO> ReadPlanCSV()
+    [HttpGet("version")]
+    public async Task<Dictionary<string, string>> GetVersion()
+    {
+        var properties = new Dictionary<string, string>();
+        var assembly = typeof(Program).Assembly;
+        properties.Add("service", "qgt-customer-service");
+        var ver = FileVersionInfo.GetVersionInfo(typeof(Program)
+        .Assembly.Location).ProductVersion;
+        properties.Add("version", ver!);
+        try
+        {
+            var hostName = System.Net.Dns.GetHostName();
+            var ips = await System.Net.Dns.GetHostAddressesAsync(hostName);
+            var ipa = ips.First().MapToIPv4().ToString();
+            properties.Add("hosted-at-address", ipa);
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex.Message);
+            properties.Add("hosted-at-address", "Could not resolve IP-address");
+        }
+        return properties;
+    }
+
+private List<PlanDTO> ReadPlanCSV()
     {
         string[] csv = System.IO.File.ReadAllLines(Path.Combine(_filepath, "Plan.csv"));
         List<PlanDTO> plan = new List<PlanDTO>();
